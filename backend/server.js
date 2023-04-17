32044c8bbdb3421afe466f7756b1de2e01858edc
Darshan <darshanparbadiya@gmail.com>
2023-04-17 18:57:45 +0530

    sample middleware code in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import colors from 'colors'
+import colors from "colors";
 
-import productRoutes from './routes/productRoutes.js'
-
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("hello");
+  next();
+});
+
 dotenv.config();
 
-connectDB()
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("service is running....");
 });
 
-app.use('/api/products',productRoutes)
-
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`server is running on port ${PORT}`.yellow.bold));
