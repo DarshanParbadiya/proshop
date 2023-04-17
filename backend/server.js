1ca7f3a44a5429231cffa14ada28279c8872612c
Darshan <darshanparbadiya@gmail.com>
2023-04-17 18:36:24 +0530

    added backend
    used several node packages express-async-handler, bcrypt.js. also created Models for db, included scripts in package.json for deleting and destroying data in the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
+
 
 const app = express();
 
@@ -14,14 +16,8 @@ app.get("/", (req, res) => {
   res.send("service is running....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products',productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`server is running on port ${PORT}`.yellow.bold));
